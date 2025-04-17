a79add345046b9937519372a8b5cddf19a214b68
Alexander Hill <ahill@breadpudding.dev>
2025-04-16 21:15:20 -0400

    Closer to properly booting

--- configure-image.sh
+++ configure-image.sh
@@ -1,4 +1,22 @@
 #!/bin/sh -e
+
+# User/Group Generation
+echo "root:x:0:0::/home/root:/bin/zsh" > /etc/passwd
+echo "root:x:0:root" > /etc/group
+
+# fstab Generation
+# TODO: Is the dump column still used today? ~ahill
+echo "/dev/vda2 /              xfs      defaults            1 1" > /etc/fstab
+echo "/dev/vda1 /boot          vfat     defaults            0 2" >> /etc/fstab
+echo "proc      /proc          proc     nosuid,noexec,nodev 0 0" >> /etc/fstab
+echo "sysfs     /sys           sysfs    nosuid,noexec,nodev 0 0" >> /etc/fstab
+echo "devpts    /dev/pts       devpts   gid=5,mode=620      0 0" >> /etc/fstab
+echo "tmpfs     /run           tmpfs    defaults            0 0" >> /etc/fstab
+echo "devtmpfs  /dev           devtmpfs mode=0755,nosuid    0 0" >> /etc/fstab
+echo "tmpfs     /dev/shm       tmpfs    nosuid,nodev        0 0" >> /etc/fstab
+echo "cgroup2   /sys/fs/cgroup cgroup2  nosuid,noexec,nodev 0 0" >> /etc/fstab
+
+# initramfs Generation
 mkdir -p /etc/tinyramfs
 echo "root=/dev/vda2" > /etc/tinyramfs/config
 echo "root_type=xfs" >> /etc/tinyramfs/config
